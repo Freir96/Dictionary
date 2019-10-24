5923d8b2b04a78cef9275ed42fb12d7d6452bebf
Adam Gajewski <adam.gajewski@asseco.pl>
2019-10-24 18:56:12 +0200

    add get_word function

--- First_Project/First_Project/First_Project.cpp
+++ First_Project/First_Project/First_Project.cpp
@@ -1,1 +1,35 @@
-﻿
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+
+void get_word(char* dest, FILE* pFile) {
+	char c = fgetc(pFile);
+	int index = 0;
+	while (isalpha(c)) {
+		dest[index] = c;
+		c = fgetc(pFile);
+		index++;
+	}
+}
+
+int main()
+{
+  ifstream in("file.txt");
+
+  if(!in) {
+    cout << "Cannot open input file.\n";
+    return 1;
+  }
+
+  char str[255];
+
+  while(in) {
+    in.getline(str, 255);  // delim defaults to '\n'
+    if(in) cout << str << endl;
+
+  }
+
+  in.close();
+
+  return 0;
+}
