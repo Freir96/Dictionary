4225d906edae3becd1b3ecbc47d8b1596d1ff802
Adam Gajewski <adam.gajewski@asseco.pl>
2019-11-14 19:05:06 +0100

    add translate function

--- First_Project/First_Project/First_Project.cpp
+++ First_Project/First_Project/First_Project.cpp
@@ -1,5 +1,9 @@
 ﻿#include <iostream>
 #include <fstream>
+#include <locale>
+#include <stdio.h>
+#include <ctype.h>
+#include <vector>
 using namespace std;
 
 void get_word(char* dest, FILE* pFile) {
@@ -12,9 +16,111 @@ void get_word(char* dest, FILE* pFile) {
 	}
 }
 
+std::string first_letter_toupper(std::string s) {
+	std::string buf = "";
+	std::string::iterator it = s.begin();
+	buf += toupper(*it);
+	++it;
+	for (; it != s.end(); ++it) {
+		buf += *it;
+	}
+
+	return buf;
+}
+
+class element {
+public: 
+	std::string pol;
+	std::string eng;
+};
+
+std::string find_translation(std::string to_translate, std::vector<element> dict ) {
+	for (element n : dict) {
+		if (n.pol == to_translate) {
+			return n.eng;
+		}
+	}
+	return to_translate;
+}
+
+std::string replace_word(std::string in, std::string to_replace, std::string new_word) {
+	std::locale loc;
+	std::string result = "";
+	std::string word = "";
+	std::string capital_word1 = first_letter_toupper(to_replace);
+	std::string capital_word2 = first_letter_toupper(new_word);
+	for (std::string::iterator it = in.begin(); it != in.end(); ++it)
+	{
+		if (std::isalpha(*it, loc))
+			word += *it;
+		else {
+			if (word == to_replace) {
+				//std::cout << word << " " << result << std::endl;
+				result += new_word;
+				word = "";
+			}			
+			else {
+				//std::cout << word << "-" << result << std::endl;
+				if (word == capital_word1) {
+					result += capital_word2;
+					word = "";
+				}
+				else {
+					result += word;
+					word = "";
+				}
+				
+				//std::cout << word << "---" << result << std::endl;
+			}
+			result += *it;
+		}
+			
+				
+	}
+	return result;
+}
+
+std::string translate(std::string in, std::vector<element> dict) {
+	std::locale loc;
+	std::string result = "";
+	std::string word = "";
+	//std::string capital_word1 = first_letter_toupper(to_replace);
+	//std::string capital_word2 = first_letter_toupper(new_word);
+	bool upper = false;
+	for (std::string::iterator it = in.begin(); it != in.end(); ++it)
+	{
+		if (std::isalpha(*it, loc)) {
+			if (std::isupper(*it, loc)) {
+				word += tolower(*it);
+				upper = true;
+			}
+			else {
+				word += *it;
+			}
+		}
+			
+		else {
+			if (upper) {
+				result += first_letter_toupper(find_translation(word, dict));
+			}
+			else {
+				std::cout << " :" << word << std::endl;
+				result += find_translation(word, dict);
+			}
+				
+			word = "";
+			upper = false;
+			result += *it;
+		}
+
+
+	}
+	return result;
+}
+
 int main()
 {
-  ifstream in("file.txt");
+ /* ifstream in("file.txt");
 
   if(!in) {
     cout << "Cannot open input file.\n";
@@ -29,7 +135,22 @@ int main()
 
   }
 
-  in.close();
+  in.close();*/
+	std::string s = "This is my cat Mark. My cats are Tim and Rick. Cat is an animal.";
+	std::string s2 = "To jest moj kot. Kot to nie pies.";
+	std::string to_replace = "cat";
+	std::string new_word = "dog";
+	//std::cout << replace_word(s, to_replace, new_word) << std::endl;
+	//std::cout << first_letter_toupper("cat\n");
+	element e1;
+	e1.eng = "dog";
+	e1.pol = "pies";
+	element e2;
+	e2.eng = "cat";
+	e2.pol = "kot";
+	std:vector <element> dict = { e1, e2 };
+	std::cout << translate(s2, dict) << std::endl;
+
 
   return 0;
 }
